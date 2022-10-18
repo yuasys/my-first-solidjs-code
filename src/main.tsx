e4bdd53147496c1f07b3dd64cda1affd2a027b60
Nob Yuasys <yuasys@gmail.com>
2022-10-18 18:25:11 +0900

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+// Nob Yuasys
+import { render } from "solid-js/web";
+import { createSignal } from "solid-js";
+
+function Counter() {
+  const [count, setCount] = createSignal(0);
+  const increment = () => setCount(count() + 1);
+
+  return (
+    <>
+      <h2>はじめて書いたSolidJSコードです</h2>
+      <button type="button" onClick={increment}>
+        {count()}
+      </button>
+    </>
+  );
+}
+
+render(() => <Counter />, document.getElementById("app")!);
